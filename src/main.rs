8e3b8a037053d23ef4cf5e0edfce0e5657693675
Jasper Bekkers <bekkers@gmail.com>
2019-03-16 16:15:13 +0100

    Add options to Generate()

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,91 @@ struct MeshDecl {
     face_ignore_data: Vec<bool>,
 }
 
+struct ChartOptions {
+    proxy_fit_metric_weight: f32,
+    roundness_metric_weight: f32,
+    straightness_metric_weight: f32,
+    normal_seam_metric_weight: f32,
+    texture_seam_metric_weight: f32,
+    max_chart_area: f32,
+    max_boundary_length: f32,
+    max_threshold: f32,
+    grow_face_count: u32,
+    max_iterations: u32,
+}
+
+impl ChartOptions {
+    fn convert(&self) -> xatlas::ChartOptions {
+        xatlas::ChartOptions {
+            proxyFitMetricWeight: self.proxy_fit_metric_weight,
+            roundnessMetricWeight: self.roundness_metric_weight,
+            straightnessMetricWeight: self.straightness_metric_weight,
+            normalSeamMetricWeight: self.normal_seam_metric_weight,
+            textureSeamMetricWeight: self.texture_seam_metric_weight,
+            maxChartArea: self.max_chart_area,
+            maxBoundaryLength: self.max_boundary_length,
+            maxThreshold: self.max_threshold,
+            growFaceCount: self.grow_face_count,
+            maxIterations: self.max_iterations,
+        }
+    }
+}
+
+impl Default for ChartOptions {
+    fn default() -> ChartOptions {
+        ChartOptions {
+            proxy_fit_metric_weight: 2.0,
+            roundness_metric_weight: 0.01,
+            straightness_metric_weight: 6.0,
+            normal_seam_metric_weight: 4.0,
+            texture_seam_metric_weight: 0.5,
+            max_chart_area: 0.0,
+            max_boundary_length: 0.0,
+            max_threshold: 2.0,
+            grow_face_count: 32,
+            max_iterations: 1,
+        }
+    }
+}
+
+struct PackOptions {
+    pub attempts: i32,
+    pub texels_per_unit: f32,
+    pub resolution: u32,
+    pub max_chart_size: u32,
+    pub block_align: bool,
+    pub conservative: bool,
+    pub padding: i32,
+}
+
+impl PackOptions {
+    fn convert(&self) -> xatlas::PackOptions {
+        xatlas::PackOptions {
+            attempts: self.attempts,
+            texelsPerUnit: self.texels_per_unit,
+            resolution: self.resolution,
+            maxChartSize: self.max_chart_size,
+            blockAlign: self.block_align,
+            conservative: self.conservative,
+            padding: self.padding,
+        }
+    }
+}
+
+impl Default for PackOptions {
+    fn default() -> PackOptions {
+        PackOptions {
+            attempts: 4096,
+            texels_per_unit: 0.0,
+            resolution: 0,
+            max_chart_size: 1024,
+            block_align: false,
+            conservative: false,
+            padding: 0,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Chart<'a> {
     atlas_index: u32,
@@ -72,6 +157,20 @@ struct Mesh<'a> {
 #[derive(Debug)]
 struct Xatlas {
     handle: *mut root::xatlas::Atlas,
+}
+
+unsafe extern "C" fn progress_cb(
+    category: root::xatlas::ProgressCategory_Enum,
+    progress: ::std::os::raw::c_int,
+    user_data: *mut ::std::os::raw::c_void,
+) {
+    let cb: *mut &mut FnMut(ProgressCategory, i32) = unsafe { std::mem::transmute(user_data) };
+    (*cb)(match category {
+        ProgressCategory_Enum_ComputeCharts => ProgressCategory::ComputeCharts,
+        ProgressCategory_Enum_ParameterizeCharts => ProgressCategory::ParameterizeCharts,
+        ProgressCategory_Enum_PackCharts => ProgressCategory::PackCharts,
+        ProgressCategory_Enum_BuildOutputMeshes => ProgressCategory::BuildOutputMeshes,
+    }, progress);
 }
 
 impl<'a> Xatlas {
@@ -121,72 +220,41 @@ impl<'a> Xatlas {
         }
     }
 
-    pub fn generate<F>(&self, mut progress: Option<F>) 
+    pub fn generate_simple(&self, chart_opts: ChartOptions, pack_opts: PackOptions){
+        let chart_opts = chart_opts.convert();
+        let pack_opts = pack_opts.convert();
+
+        unsafe {
+            xatlas::Generate(
+                self.handle,
+                chart_opts,
+                None,
+                pack_opts,
+                None,
+                std::ptr::null_mut(),
+            )
+        }
+    }
+
+    pub fn generate<F>(&self, chart_opts: ChartOptions, pack_opts: PackOptions, mut progress: F) 
     where
         F: FnMut(ProgressCategory, i32),
     {
-        let chart_ops = xatlas::ChartOptions {
-            proxyFitMetricWeight: 2.0,
-            roundnessMetricWeight: 0.01,
-            straightnessMetricWeight: 6.0,
-            normalSeamMetricWeight: 4.0,
-            textureSeamMetricWeight: 0.5,
-            maxChartArea: 0.0,
-            maxBoundaryLength: 0.0,
-            maxThreshold: 2.0,
-            growFaceCount: 32,
-            maxIterations: 1,
-        };
+        let chart_opts = chart_opts.convert();
+        let pack_opts = pack_opts.convert();
 
-        let pack_opts = xatlas::PackOptions {
-            attempts: 4096,
-            texelsPerUnit: 0.0,
-            resolution: 0,
-            maxChartSize: 1024,
-            blockAlign: false,
-            conservative: false,
-            padding: 0,
-        };
+        let mut cb: &mut FnMut(ProgressCategory, i32) = &mut progress;
+        let cb = &mut cb as *mut &mut FnMut(ProgressCategory, i32) as *mut std::ffi::c_void;
 
-        unsafe extern "C" fn progress_cb(
-            category: root::xatlas::ProgressCategory_Enum,
-            progress: ::std::os::raw::c_int,
-            user_data: *mut ::std::os::raw::c_void,
-        ) {
-            let cb: *mut &mut FnMut(ProgressCategory, i32) = unsafe { std::mem::transmute(user_data) };
-            (*cb)(match category {
-                ProgressCategory_Enum_ComputeCharts => ProgressCategory::ComputeCharts,
-                ProgressCategory_Enum_ParameterizeCharts => ProgressCategory::ParameterizeCharts,
-                ProgressCategory_Enum_PackCharts => ProgressCategory::PackCharts,
-                ProgressCategory_Enum_BuildOutputMeshes => ProgressCategory::BuildOutputMeshes,
-            }, progress);
-        }
-
-        if let Some(mut progress) = progress {
-            let mut cb: &mut FnMut(ProgressCategory, i32) = &mut progress;
-            let cb = &mut cb as *mut &mut FnMut(ProgressCategory, i32);
-
-            unsafe {
-                xatlas::Generate(
-                    self.handle,
-                    chart_ops,
-                    None,
-                    pack_opts,
-                    Some(progress_cb),
-                    cb as *mut std::ffi::c_void,
-                )
-            }
-        } else {
-            unsafe {
-                xatlas::Generate(
-                    self.handle,
-                    chart_ops,
-                    None,
-                    pack_opts,
-                    None,
-                    std::ptr::null_mut(),
-                )
-            }
+        unsafe {
+            xatlas::Generate(
+                self.handle,
+                chart_opts,
+                None,
+                pack_opts,
+                Some(progress_cb),
+                cb
+            )
         }
     }
 
@@ -252,7 +320,8 @@ fn main() {
     decl.index_format = IndexFormat::Uint32;
 
     atlas.add_mesh(&decl);
-    atlas.generate(Some(|_, p|{println!("Hi {}", p)}));
+    atlas.generate_simple(Default::default(), Default::default());
+
     let meshes = atlas.meshes();
 
     dbg!(meshes);
